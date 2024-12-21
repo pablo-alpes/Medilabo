e911e1622a026223df8d1128681057d36461f51d
pablo-alpes <pablo.miranda@sfr.Fr>
2024-12-21 13:23:49 +0100

    Initial commit

---
+++ src/test/java/com/medilabo/solutions/MedilaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
